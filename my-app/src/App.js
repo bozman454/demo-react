b950e81ac3b397ed3b2f269a17073509985bdcfc
bozman454 <35748440+bozman454@users.noreply.github.com>
2020-06-29 09:41:51 -0400

    added state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        Hello world, IM AN APP COMPONENT!
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {data: "DATA"};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Hello world, IM AN APP COMPONENT!
+
+          {this.state.data}
       </h1>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
