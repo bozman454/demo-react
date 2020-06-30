7d170b5101fd879544d8732ace58a3e41431f921
bozman454 <35748440+bozman454@users.noreply.github.com>
2020-06-30 09:41:51 -0400

    added event handling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,13 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {datalist: props.datalist};
+    this.handleClick = this.handleClick.bind(this)
   }
+
+  handleClick(){
+    this.setState({datalist: ['i', 'changed', 'it']})
+  }
+
 
   render() {
     return (
@@ -14,6 +20,9 @@ class App extends React.Component {
         <h1>
           Hello world, IM passing props!
       </h1>
+      <h2>
+        <input type="button" onClick={this.handleClick}/>
+      </h2>
       <ul>
         {this.state.datalist.map(element =>{
             return <li> {element} </li>
@@ -23,5 +32,7 @@ class App extends React.Component {
     );
   }
 }
+
+
 
 export default App;
